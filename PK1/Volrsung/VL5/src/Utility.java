26912fa88724d84598a0eae563781bd493910031
Kamikaze74 <mikailcay13@gmail.com>
2025-05-15 12:38:56 +0200

    files

--- PK1/Volrsung/VL5/src/Utility.java
+++ PK1/Volrsung/VL5/src/Utility.java
@@ -1,11 +1,7 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
@@ -15,59 +11,52 @@ public class Utility {
 public static void main(String[] args) throws IOException{
 
     sayHello(System.out);
-    File f = new File("C:/Users/mikai/Documents/hier.txt");
-    File f1 = new File("C:/Users/mikai/Documents/da.txt");
 
-    try {
-        OutputStream otp = new FileOutputStream(f);
+    File f = new File("/home/mika/Uni/PK1/Volrsung/VL5/files/hier.txt");
+    File f1 = new File("/home/mika/Uni/PK1/Volrsung/VL5/files/da.txt");
+
+    try(OutputStream otp = new FileOutputStream(f)) {
         sayHello(otp);
         copy(f, f1);
         cat(f1);
     } catch (FileNotFoundException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
     }
-    
-    }
+}
 
- public static void sayHello(OutputStream s) throws IOException {
+public static void sayHello(OutputStream s) throws IOException {
 
- String t = "Hello World";
- OutputStreamWriter sw = new OutputStreamWriter(s);
- sw.write(t.toCharArray());
- sw.flush();
+    String t = "Hello World";
+    OutputStreamWriter sw = new OutputStreamWriter(s);
+    sw.write(t.toCharArray());
+    sw.flush();
     }
 
 static void cat(File quelle){
 
-        try( RandomAccessFile raf = new RandomAccessFile(quelle, "r")) {
+    try( RandomAccessFile raf = new RandomAccessFile(quelle, "r")) {
             
-            int b = 0;
-            {
-            while ((b = raf.read()) != -1) 
-               System.out.print((char) b);
-               
+        int c = 0;
 
-    } 
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-     
+        while ((c = raf.read()) != -1)
+        System.out.print((char) c);
+    
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 }
 
 public static void copy(File from, File to) {
+    
     try (
         RandomAccessFile in = new RandomAccessFile(from, "r");
-        RandomAccessFile out = new RandomAccessFile(to, "rw")
+        RandomAccessFile out = new RandomAccessFile(to, "rw");
     ) {
-   
+
     int c;
-    while ((c = in.read()) != -1) 
+    while ((c = in.read()) != -1)
         out.write(c);
     
     } catch (FileNotFoundException e) {
