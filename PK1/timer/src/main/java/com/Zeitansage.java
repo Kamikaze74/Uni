2b1776d00dbb28fff33b4964a7853857a2f09abb
Kamikaze74 <mikailcay13@gmail.com>
2025-07-08 22:58:33 +0200

    timer fertig

--- PK1/timer/src/main/java/com/Zeitansage.java
+++ PK1/timer/src/main/java/com/Zeitansage.java
@@ -1,16 +1,34 @@
 package com;
 
-public class Zeitansage{
-    public static void main(String[] args){
+import com.guiutil.pk1.gui.util.MessageView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Zeitansage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
         TimerListener listener = new TimerListener() {
             int count = 0;
+
             @Override
-            public void signalPerformed(){
+            public void signalPerformed() {
                 System.out.println(count + " Sekunden seit Start");
                 count++;
             }
         };
-        Thread a = new Thread( new Timer(listener));
+
+        Thread a = new Thread(new Timer(listener));
+        a.setDaemon(true); // beendet sich automatisch mit dem JavaFX-Thread
         a.start();
+
+        MessageView view = MessageView.create(primaryStage, "Message", "Zeitansage stoppen");
+        view.showView();
+
+        a.interrupt(); // ohne interrupt beendet sich das Programm nicht
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
